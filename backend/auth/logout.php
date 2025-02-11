ec8ee693aa06c0ad3a7fafaa65f7d92cdc75e612
backingauto <chonheic@buffalo.edu>
2025-02-11 18:36:09 -0500

    logout backend

---
+++ backend/auth/logout.php
@@ -0,0 +1,27 @@
+<?php
+header("Content-Type: application/json");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+
+include '../db_connection.php';
+
+//check if auth_token exsits
+if (isset($_COOKIE["auth_token"])) {
+    $auth_token = $_COOKIE["auth_token"];
+
+    //remove it from the db
+    $query = "UPDATE users SET auth_token = NULL WHERE auth_token = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("s", $auth_token);
+    $stmt->execute();
+    $stmt->close();
+
+    //expire the cookie
+    setcookie("auth_token", "", time()-3600);
+}
+
+echo json_encode(["success" => true, "message" => "logout successfully"]);
+
+$conn->close();
+?>
--- src/frontend/homepage/HomePage.jsx
+++ src/frontend/homepage/HomePage.jsx
@@ -1,6 +1,9 @@
 import { Link, useNavigate } from 'react-router-dom';
+import { useState } from "react";   
 
 function HomePage() {
+
+    
 
     return (
         <div>
